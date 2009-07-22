f508e09e17fd2e8f9fe4c83aeb05153ef8221cf1
unknown <es.miss.pi@gmail.com>
2009-07-22 20:49:52 +0200

    casi listo

---
+++ public/javascripts/jquery.wyslist.js
@@ -0,0 +1,35 @@
+
+$(function() {
+$('#quotes').crossSlide({
+  sleep: 5,
+  fade: 1
+}, [
+  { src: '/images/quotes/cita_01.jpg' },
+  { src: '/images/quotes/cita_02.jpg' },
+  { src: '/images/quotes/cita_03.jpg' },
+  { src: '/images/quotes/cita_04.jpg' },
+  { src: '/images/quotes/cita_05.jpg' },
+  { src: '/images/quotes/cita_06.jpg' },
+  { src: '/images/quotes/cita_08.jpg' },
+  { src: '/images/quotes/cita_10.jpg' },
+  { src: '/images/quotes/cita_11.jpg' },
+  { src: '/images/quotes/cita_12.jpg' },
+  { src: '/images/quotes/cita_13.jpg' },
+  { src: '/images/quotes/cita_14.jpg' },
+  { src: '/images/quotes/cita_15.jpg' },
+  { src: '/images/quotes/cita_16.jpg' },
+  { src: '/images/quotes/cita_17_ca.jpg' },
+  { src: '/images/quotes/cita_18_ca.jpg' },
+  { src: '/images/quotes/cita_19_ca.jpg' },
+  { src: '/images/quotes/cita_20_ca.jpg' },
+  { src: '/images/quotes/cita_21_ca.jpg' },
+  { src: '/images/quotes/cita_22_ca.jpg' },
+  { src: '/images/quotes/cita_23_ca.jpg' },
+  { src: '/images/quotes/cita_24_ca.jpg' },
+  { src: '/images/quotes/cita_25_ca.jpg' },
+  { src: '/images/quotes/cita_26_ca.jpg' }
+  
+]);
+
+
+});
