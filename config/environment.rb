8e84061bd6420b9b7dc5e8f86a73869da5697512
dani <danigb@gmail.com>
2010-03-24 12:32:23 +0100

    update logos, database backup

--- config/environment.rb
+++ config/environment.rb
@@ -7,7 +7,7 @@ RAILS_GEM_VERSION = '2.3.2' unless defined? RAILS_GEM_VERSION
 require File.join(File.dirname(__FILE__), 'boot')
 
 Rails::Initializer.run do |config|
-  gem 'browsercms'
+  gem 'browsercms', :version => '3.0.1'
 
 
   config.time_zone = 'UTC'
